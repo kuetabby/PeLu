8030d1c2c03103d9da8442fe37893ef054c3e100
kuetabby <vansmuhammad@gmail.com>
2024-11-27 18:14:52 +0700

    update

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -3,15 +3,15 @@ export const pairAddress = "-";
 
 export const socialsLink = {
   whitepaper: "/",
-  twitter: "https://x.com/",
-  telegram: "https://t.me/",
+  twitter: "https://x.com/DraconisSol",
+  telegram: "https://t.me/DraconisSol",
   medium: "/",
   email: "mailto:",
 };
 
 export const findUsLink = {
   dextools: `http://dextools.io/app/ether/pair-explorer/${pairAddress}`,
-  dexscreener: `https://dexscreener.com/solana/c7rprvhvw6n3kyucv8ayat5vl11pvvjsq8oge6rowgym`,
+  dexscreener: `https://dexscreener.com/solana/${contractAddress}`,
   solidproof: "https://app.solidproof.io/projects/",
   etherscan: `https://etherscan.io/token/${contractAddress}`,
   uniswap: `https://app.uniswap.org/tokens/ethereum/${contractAddress}`,
